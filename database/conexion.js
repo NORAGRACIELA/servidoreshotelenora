85e17e05ce89437d70fa7da57fc77c653af481d4
201 <201@academicos.cesde.edu.co>
2022-08-30 20:48:15 -0500

    ok

--- database/conexion.js
+++ database/conexion.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 //ME CONECTO A LA BASE DATOS
 export async function conectar (){
     try{
-        await mongoose.connect('process.env.DATABASE');
+        await mongoose.connect(process.env.DATABASE);
     }catch(error){
 
         console.log("Upss"+error)
